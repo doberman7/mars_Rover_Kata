4e9658abf38d01e55be184591cc3dd6e190633bc
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-09-22 19:26:07 -0500

    1st comment

---
+++ js-Mars-Rover_Kata.js
@@ -0,0 +1,22 @@
+//create a function to turn the rover,
+//create a function to move the rover forward or backward based on its direction,
+//create a function to receive a list of commands and move based on those commands.
+
+// Rover object goes here:
+let rover = {
+  direction: ""
+}
+
+// ======================
+function turnLeft(rover) {
+  console.log('turnLeft was called!');
+
+}
+
+function turnRight(rover) {
+  console.log('turnRight was called!');
+}
+
+function moveForward(rover) {
+  console.log('moveForward was called');
+}
