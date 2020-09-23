6ccd965119117a010628013f6e39b5e1d1415167
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-09-23 15:04:35 -0500

    move forward imnplemented

--- js-Mars-Rover_Kata.js
+++ js-Mars-Rover_Kata.js
@@ -2,14 +2,15 @@
 //create a function to move the rover forward or backward based on its direction,
 //create a function to receive a list of commands and move based on those commands.
 //The direction property can contain one of four values: "N", "S", "E", or "W". The rover’s default direction will be "N" (as North).
-let log = (print)=>{
+let log = (print) => {
   return console.log(print);
 };
 
 // Rover object goes here:
 let rover = {
   direction: "N",
-  // position: [0,0],
+  //         [x,y]
+  position: [0, 0],
 };
 
 // ======================
@@ -20,9 +21,9 @@ let rover = {
 
 function turnLeft(rover) {
   log('turnLeft was called!');
-  switch(rover.direction) {
+  switch (rover.direction) {
     case 'N':
-     log(" Rover is now facing West");
+      log(" Rover is now facing West");
       // rover.position[0]++;
       break;
     case 'E':
@@ -44,13 +45,32 @@ function turnLeft(rover) {
 //   console.log('turnRight was called!');
 // };
 //
-// function moveForward(rover) {
-//   console.log('moveForward was called');
-// };
-//
+function moveForward(rover) {
+  // log('moveForward was called');
+  switch (rover.direction) {
+    case 'N': //If the rover is facing north and moves forward, we would encrease the rover’s y by 1.
+      rover.position[1]++;
+      log(" Rover is now facing North at "+ rover.position);
+      break;
+    case 'E'://If the rover is facing north and moves forward, we would encrease the rover’s y by 1.
+      rover.position[0]++;
+      log("Rover is now facing East at "+ rover.position);
+      break;
+    case 'S': //If the rover is facing south and moves forward, we would decrease the y by 1.
+      rover.position[1]--;
+      log("Rover is now facing South at "+ rover.position);
+      break;
+    case 'W': //if the rover is facing west and moves forward, we would decrease the rover’s x by 1.
+      rover.position[0]--;
+      log("Rover is now facing West at "+ rover.position);
+      break;
+  }
+};
+
 // function moveBackward(rover) {
 //   console.log('moveForward was called');
 // };
 
 
-turnLeft(rover);
+// turnLeft(rover);
+moveForward(rover);
